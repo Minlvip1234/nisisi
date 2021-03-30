e8b6a4222bf9228138a379298ba3bb79c275077e
Minh <anhddgch18611@fpt.edu.vn>
2021-03-30 16:58:04 +0700

    commit

--- sjsd.js
+++ sjsd.js
@@ -1,1 +1,18 @@
-jshdjasd
+var express = require('express')
+var app = express();
+var hbs =require('hbs');
+
+app.set('view engine', 'hbs')
+
+var Image = require('path').join(__dirname,'/Image');
+app.use(express.static(Image))
+
+app.get('/', (req,res)=> {res.write("nsnjsdnsjd")
+res.end()
+})
+app.get('/AccountPage', (req,res)=> {res.render('AccountPage')})
+app.get('/InformationPage', (req,res)=> {res.render('InformationPage')})
+
+
+app.listen(5000);
+console.log('Node.js web server at port 5000 is running..')
